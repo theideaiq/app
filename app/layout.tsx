a7a3b0170e3f659aacad9388a2885688f03eaede
Shaheen K. Farjo <shaheen.farjo@gmail.com>
2026-01-03 12:07:42 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${poppins.variable} font-sans antialiased`}>
-        <Toastprovider />
+        <ToastProvider />
         <Navbar />
         {children}
         <Footer />
